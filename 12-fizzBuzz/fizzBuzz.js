c33f5277395647405bc93801fc99792cee725fbd
Daniel Targonski <daniel.targonski2@gmail.com>
2020-12-31 00:14:45 -0500

    completed projects up to 12

--- 12-fizzBuzz/fizzBuzz.js
+++ 12-fizzBuzz/fizzBuzz.js
@@ -1,6 +1,21 @@
 function fizzBuzz(start, end) {
-  // Insert code here;
-}
+  var fizzArr = [], index = 0;
 
+  for (let i = start; i <= end; i++) {
+    if (i % 3 == 0)
+    fizzArr[index] = "Fizz";
+    else if (i % 5 == 0)
+    fizzArr[index] = "Buzz";
+    else
+    fizzArr[index] = i;
+
+    if (i % 3 == 0 && i % 5 == 0)
+    fizzArr[index] = "FizzBuzz";
+    
+    index++;
+  }
+  return fizzArr;
+}
+console.log(fizzBuzz(1, 20));
 // Do not edit this line;
 module.exports = fizzBuzz;
