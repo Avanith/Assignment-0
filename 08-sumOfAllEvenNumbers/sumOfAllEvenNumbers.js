0692bd1fc0d488afdb1f44df2ae73fc348227106
Daniel Targonski <daniel.targonski2@gmail.com>
2020-12-30 22:45:53 -0500

    Completed proj 08

--- 08-sumOfAllEvenNumbers/sumOfAllEvenNumbers.js
+++ 08-sumOfAllEvenNumbers/sumOfAllEvenNumbers.js
@@ -1,5 +1,10 @@
 function sumOfAllEvenNumbers(nums) {
-  // Insert code here;
+  evenNumberSumTotal = 0;
+  nums.forEach(element => {
+    if (element % 2 == 0 || element == 0)
+      evenNumberSumTotal++
+  });
+  return evenNumberSumTotal;
 }
 
 // Do not edit this line;
