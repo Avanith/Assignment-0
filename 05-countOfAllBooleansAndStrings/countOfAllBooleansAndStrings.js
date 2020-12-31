fcf4dc570c4059f5bf9b96f2e2bd7d8dfeefe2e5
Daniel Targonski <daniel.targonski2@gmail.com>
2020-12-30 19:50:02 -0500

    Project 5 complete

--- 05-countOfAllBooleansAndStrings/countOfAllBooleansAndStrings.js
+++ 05-countOfAllBooleansAndStrings/countOfAllBooleansAndStrings.js
@@ -1,6 +1,17 @@
 function countOfAllBooleansAndStrings(arr) {
-  // Insert code here;
+  boolCounter = new Number(0);
+
+  // a bool put through the toString.call() method returns '[object Boolean]'
+  // we use this to check if each element in the array is a boolean
+  arr.forEach(element => {
+    if (toString.call(element) === '[object Boolean]' || typeof element == 'string')
+      boolCounter++;
+  });
+  //console.log(isFinite(boolCounter));
+  return boolCounter;
 }
+arr = [8, "hello", undefined, null, false, false, false, false, false];
+console.log(countOfAllBooleansAndStrings(arr));
 
 // Do not edit this line;
 module.exports = countOfAllBooleansAndStrings;
