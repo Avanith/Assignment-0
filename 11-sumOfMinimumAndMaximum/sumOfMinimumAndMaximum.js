f2012d6989944d762df5b9980a1fb2e2770527c5
Daniel Targonski <daniel.targonski2@gmail.com>
2020-12-30 23:38:46 -0500

    proj 11 complete

--- 11-sumOfMinimumAndMaximum/sumOfMinimumAndMaximum.js
+++ 11-sumOfMinimumAndMaximum/sumOfMinimumAndMaximum.js
@@ -1,5 +1,13 @@
 function sumOfMinimumAndMaximum(nums) {
-  // Insert code here;
+  var sum = 0, min = nums[0], max = nums[0];
+  for (let i = 0; i < nums.length; i++)
+  {
+    if (min > nums[i])
+    min = nums[i];
+    if (max < nums[i])
+    max = nums[i];
+  }
+  return max + min;
 }
 
 // Do not edit this line;
