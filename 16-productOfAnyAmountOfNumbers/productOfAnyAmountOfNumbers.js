64c2e7e1e044db94aa4ed2b6c078f6acf23b4d9c
Daniel Targonski <daniel.targonski2@gmail.com>
2020-12-31 19:13:45 -0500

    p16 complt

--- 16-productOfAnyAmountOfNumbers/productOfAnyAmountOfNumbers.js
+++ 16-productOfAnyAmountOfNumbers/productOfAnyAmountOfNumbers.js
@@ -1,6 +1,13 @@
 function productOfAnyAmountOfNumbers(...args) {
-  // Insert code here;
-  
+  let productTotal = args[0];
+  // loop through the argument array to trigger the fe
+  if (args.length > 1)
+  for (let i = 1; i < args.length; i++)
+productTotal*= args[i];
+  else
+    return args[0];
+
+  return productTotal;
 }
 
 // Do not edit this line;
